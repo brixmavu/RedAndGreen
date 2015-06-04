1cdea82c42e8e9aa60b6cc784e8c2e6c05de2e10
brixmavu <brixton@projectcodex.co>
2015-06-04 15:27:10 +0200

    task 3 is done

---
+++ task3.js
@@ -0,0 +1,7 @@
+ function greenFunction() {
+    document.getElementById("green").style.backgroundColor = "green";
+}
+	function redFunction() {
+    document.getElementById("red").style.backgroundColor = "red";
+
+}
