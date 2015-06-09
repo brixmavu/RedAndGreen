d23bdab0652f3659285885d14c058515b62f2d18
brixmavu <brixton@projectcodex.co>
2015-06-09 10:24:44 +0200

    Task 4 is done

---
+++ task4.js
@@ -0,0 +1,23 @@
+var i=0;
+    var j =["#008000", "#FF0000", "#008000", "#FF0000"];
+        function changeColor() {
+            if(i>=j.length-1){
+                i=0;
+            }
+            else{
+                i++;
+            }
+            var col=document.getElementById("square");
+            col.style.backgroundColor = j[i];
+
+
+            var el = document.getElementById("colorChange");
+                if (el.firstChild.data == "Make Green") 
+            {
+                el.firstChild.data = "Make Red";
+            }
+                else 
+            {
+                el.firstChild.data = "Make Green";
+            }
+    }
