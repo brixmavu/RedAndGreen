3eb61f9b949fad0aef290d75f25d09d8e0092ffa
brixmavu <brixton@projectcodex.co>
2015-06-15 17:12:07 +0200

    Changes in task 6

--- red_green.js
+++ red_green.js
@@ -2,10 +2,12 @@ function RedOrGreen(square){
 
    this.makeGreen = function(){
    		document.getElementById("square").style.backgroundColor = "green";
+   		
    }
    
  	this.makeRed = function(){
    		document.getElementById("square").style.backgroundColor = "red";
+   		
    }
 
 }
--- red_green_test.js
+++ red_green_test.js
@@ -1,7 +1,20 @@
 	var rg = new RedOrGreen("square");
 
-   rg.makeGreen();
-   alert("green");
+   rg.makeGreen();  
+   if(document.getElementById('square').style.backgroundColor === 'green'){
+   		alert("success");	
+   }
+   else{
+   		alert("failure");
+   }
+   
 
    rg.makeRed();
-   alert("red");
+   if(document.getElementById('square').style.backgroundColor === 'red'){
+         alert("success"); 
+   }
+   else{
+         alert("failure");
+   }
+   
+   //(backgroundColor == "red") ? true : false;
